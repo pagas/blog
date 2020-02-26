81fd4cd26dce361192a89f1a0de47d8dc9720744
paulius <paulius@2simple.com>
2020-02-26 21:22:20 +0000

    fix post schema

--- server/src/models/post.ts
+++ server/src/models/post.ts
@@ -4,12 +4,12 @@ import {IPostDocument} from './interfaces/IPostDocument';
 export interface IPost extends IPostDocument {
 }
 
-const ArticleSchema = new Schema({
+const PostSchema = new Schema({
     title: {type: String, required: true, unique: true, trim: true},
     body: {type: String, required: true},
-    authorId: {type: String, required: true},
-    created: {type: Date, default: Date.now, ref: 'User', required: true}
+    authorId: {type: String, required: true, ref: 'User'},
+    created: {type: Date, default: Date.now}
 });
 
-const Post = model<IPost>('Post', ArticleSchema);
+const Post = model<IPost>('Post', PostSchema);
 export {Post};
